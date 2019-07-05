a56e6191d4ab5c317672d01fe844182c51a708e0
JF Z <a378789308@gmail.com>
2019-07-05 14:52:03 +0800

    更新目录

--- VerifyCodeDemo/VerifyCodeView/TDWExtension/TDWExtensions.swift
+++ VerifyCodeDemo/VerifyCodeView/TDWExtension/TDWExtensions.swift
@@ -1,16 +1,28 @@
 //
-//  UIColorExtension.swift
-//  TuanDaiV4
+//  TDWUIScreenExtensions.swift
+//  TDWCashLoan
 //
-//  Created by AndreaArlex on 2017/7/13.
-//  Copyright © 2017年 Dee. All rights reserved.
+//  Created by ZhengRuSong on 2017/8/4.
+//  Copyright © 2017年 com.tuandaiwang.www. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
-public extension UIColor {
+public extension UIScreen {
+    static func tdw_width() -> CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    static func tdw_height() -> CGFloat {
+        return UIScreen.main.bounds.height
+    }
+    static func tdw_size() -> CGSize {
+        return UIScreen.main.bounds.size
+    }
+}
 
+public extension UIColor {
+    
     //用数值初始化颜色，便于生成设计图上标明的十六进制颜色
     convenience init(TDWValueRGB: UInt, alpha: CGFloat = 1.0) {
         self.init(
